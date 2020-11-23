13293dcc8c4db664e063ab4b3206c4372671aadc
SuayMack <pry_chu@homail.com>
2020-11-23 15:40:35 -0300

    update commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,75 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule, LOCALE_ID } from '@angular/core'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatButtonModule} from '@angular/material/button'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { HttpClientModule } from  '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/template/header/header.component'
+import { FooterComponent } from './components/template/footer/footer.component'
+import { NavComponent } from './components/template/nav/nav.component'
+import { HomeComponent } from './views/home/home.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { RedDirective } from './directives/red.directive'
+import { ForDirective } from './directives/for.directive'
+import { ProductCreateComponent } from './components/product/product-create/product-create.component'
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component,
+    ProductUpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule    
+  ],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {  
+  
+  baseUrl = "http://localhost:3001/prducts"
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product);
+  }
+}
