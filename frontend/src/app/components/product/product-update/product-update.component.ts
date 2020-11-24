86f0d7cf2e33833e6219e15ef11a047f8735cb10
SuayMack <pry_chu@hotmail.com>
2020-11-23 21:32:14 -0300

    create product-read

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  updateProduct(): void {
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
